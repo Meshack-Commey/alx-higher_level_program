81c8a36c25d98ebf64351f4501e1595244cc2033
Meshack-Commey <commeymeshack@gmail.com>
2022-10-04 04:56:18 +0000

    JavaScrip Warm up

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+// Uses an array to loop through and print strings
+
+const languages = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+for (let i = 0; i < languages.length; i++) {
+  console.log(languages[i]);
+}
